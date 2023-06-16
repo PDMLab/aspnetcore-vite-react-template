8541042f5c5ff533bf47d14e1077246322cb44f4
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2023-06-16 23:55:49 +0200

    feat: run vite react-ts from wwwroot folder in Index.cshtml view

---
+++ src/ViteAspNetReact/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// var options = new DefaultFilesOptions();
+// options.DefaultFileNames.Clear();
+// options.DefaultFileNames.Add("default.html");
+
+
+// app.UseDefaultFiles(options);
+app.UseStaticFiles();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.Run();
