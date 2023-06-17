38f4d7dae230c01688b70aa4679ec8f21f8ed16a
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2023-06-17 12:41:44 +0200

    feat: get data from API in frontend

--- src/ViteAspNetReact/wwwroot/src/App.tsx
+++ src/ViteAspNetReact/wwwroot/src/App.tsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [greeting, setGreeting] = useState('');
+
+  let initialized = false;
+
+  useEffect(() => {
+    if (!initialized) {
+      initialized = true
+      const fetchData = async () => {
+        const response = await fetch('api/hello');
+        const data = await response.json() as { hello: string };
+        console.log(data);
+        setGreeting(data.hello);
+      };
+      fetchData();
+    }
+  }, []);
+
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
       <h1>Vite + React + ASP.NET Core</h1>
@@ -24,6 +41,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+      </div>
+      <div>
+        <p>hello, <span className={'text-green-400'}>{greeting}</span></p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
